1ddf2cf435034c026ffd1e70954451848d94729c
benjaminGUYT <55363637+benjaminGUYT@users.noreply.github.com>
2020-03-26 00:03:24 +0100

    Update Scipt.sql
    Mis en place de la table evenement, galere sur le trigger mysql :D

--- BDD/Scipt.sql
+++ BDD/Scipt.sql
@@ -1,12 +1,13 @@
 
 SET NAMES utf8;
-DROP TABLE IF EXISTS personne;
-DROP TABLE IF EXISTS sport;
-DROP TABLE IF EXISTS equipe;
-DROP TABLE IF EXISTS invitation;
-DROP TABLE IF EXISTS appartenance;
-DROP TABLE IF EXISTS coach;
 DROP TABLE IF EXISTS message;
+DROP TABLE IF EXISTS coach;
+DROP TABLE IF EXISTS appartenance;
+DROP TABLE IF EXISTS invitation;
+DROP TABLE IF EXISTS evenement;
+DROP TABLE IF EXISTS equipe;
+DROP TABLE IF EXISTS sport;
+DROP TABLE IF EXISTS personne;
 
 -- Définition des tables avec les clef primaire et ajouts des données de test -- 
 
@@ -58,10 +59,10 @@ create table invitation (
     unique(id_equipe, id_personne)
 );
 
-lock table invitation;
+lock table invitation write;
 insert into invitation values
 (1, 3),
-(2 ,5);
+(2 ,6);
 unlock tables;
 
 create table appartenance (
@@ -70,38 +71,54 @@ create table appartenance (
     unique(id_equipe, id_personne)
 );
 
-lock table appartenance;
+lock table appartenance write;
 insert into appartenance values
 (1, 2),
-(2, 6);
+(2, 5);
 unlock tables;
 
 create table coach (
     id_equipe int(32) not null,
     id_personne int(32) not null,
-    unique(id-equipe, id_personne)
+    unique(id_equipe, id_personne)
 );
 
-lock table coach;
+lock table coach write;
 insert into coach values
 (1, 1),
-(2, 3);
+(2, 4);
 unlock tables;
 
 create table message (
     id_equipe int(32) not null,
     id_personne int(32) not null,
     temporalite datetime not null,
-    texte varchar(21844) not null
+    texte text(21844) not null
 );
 
-lock table message;
+lock table message write;
 insert into message values
 (1, 1, '2020-01-01 00:00:01', 'PUTAIN DE MERDE'),
-(1, 1, '2020-04-04 00:00:20', 'J4AI AJOUTE MON TETON GAUCHE SUR MON CAPTEUR DIGITALE');
-(1, 1, '2020-04-04 00:00:52', 'ET CA FONCTIONNE');
-(2, 4, '2020-01-01 00:00:01', 'Bonne année les freros'),
-(2, 3, '2020-04-04 12:12:12', 'Ok ..');
+(1, 1, '2020-04-04 00:00:20', 'J4AI AJOUTE MON TETON GAUCHE SUR MON CAPTEUR DIGITALE'),
+(1, 1, '2020-04-04 00:00:52', 'ET CA FONCTIONNE'),
+(2, 5, '2020-01-01 00:00:01', 'Bonne année les freros'),
+(2, 4, '2020-04-04 12:12:12', 'Ok ..');
+unlock tables;
+
+create table evenement (
+    id_equipe int(32) not null,
+    id_personne int(32) not null,
+    temporalite_debut datetime not null,
+    temporalite_fin datetime not null,
+    texte varchar(30) default null
+);
+
+lock table evenement write;
+insert into evenement values
+(1, 1, '2020-01-01 00:00:01', '2020-01-01 00:00:02', 'Ceci est une seconde'),
+(1, 1, '2020-01-01 00:01:00', '2020-01-01 00:02:00', 'Ceci est une minute'),
+(1, 1, '2020-01-01 01:00:00', '2020-01-01 02:00:00', 'Ceci est une heure'),
+(1, 1, '2020-01-01 02:00:01', '2020-01-01 03:30:01', 'Ceci est une heure et demie');
 unlock tables;
 
 -- Définition des clefs secondaires --
@@ -134,3 +151,21 @@ alter table coach
     add constraint fk_coach_personne
         foreign key (id_personne) references personne(id);
 
+-- Définition des triggers --
+
+CREATE TRIGGER tr_evenement_check
+BEFORE INSERT ON evenement FOR EACH ROW
+
+-- EN TEST --
+BEGIN
+	DECLARE @v_message varchar(30);
+    SET @ v_message = ( SELECT message FROM evenement WHERE id_equipe = :NEW.id_equipe AND ((temporalite_debut > :NEW.temporalite_debut AND temporalite_debut < :NEW.temporalite_fin)OR (temporalite_fin > :NEW.temporalite_debut AND temporalite_fin < :NEW.temporalite_fin)));
+    IF (SELECT COUNT(*) FROM test) <= 0 THEN
+    END IF;
+END;
+
+lock table evenement write;
+insert into evenement values
+(1, 1, '2020-01-01 02:00:02', '2020-01-01 03:30:02', 'Ceci est un fail'),
+(1, 1, '2020-01-01 01:00:02', '2020-01-01 03:29:59', 'Pour etre sûr');
+unlock tables;
