45382ffe696bb20c792033b6a9403dfd487fffdf
benjaminGUYT <55363637+benjaminGUYT@users.noreply.github.com>
2020-03-26 22:45:57 +0100

    Update PersonneRoute.js
    Mise en place de la fonction getbyid

--- api/routes/PersonneRoute.js
+++ api/routes/PersonneRoute.js
@@ -37,7 +37,7 @@ router.post('/inscription', async(req, res, next) => {
             return res.status(400).json(erreur(400, 'mail déjà répertorié'));
         }
     } catch (e) {
-        return res.status(500).json(error(500,e));
+        return res.status(500).json(error(500, e));
     }
 
     /* On crypte le mot de passe, puis en envoie la requête à la BDD */
@@ -100,22 +100,26 @@ router.post('/login', async(req, res, next) => {
 
 /* ------------------------- Définition des routes AVEC JWT ------------------------- */
 
-router.get('/:id', async(req, res, next) => {
-    
+router.get('/get/:id', async(req, res, next) => {
+
     var token = req.headers['authorization'];
     /*  On stock l'identifiant de l'utilisateur contenu dans le token */
     var id = jwtUtils.checkToken(token);
     if (id < 0) {
-        return res.status(400).json(error(400, 'Token invalide' ));
+        return res.status(400).json(error(400, 'Token invalide'));
     }
 
     try {
-        let results = await model.getPersonneById(req.params.id)
+        let id = req.params.id.split('=');
+        id = id[1];
+        let results = await model.getPersonneById(id)
         res.json(results);
     } catch (e) {
         /* Le message d'erreur devra renvoyer le message sql lisible et sans informations sur la BDD */
         return res.status(500).json(error(500, e));
     }
 })
+
+
 
 module.exports = router
