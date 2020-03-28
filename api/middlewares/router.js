357a7266b2c303b23c057e2be42cc2fbacdb103e
Oliwier Maziarz <maziarzoliwier93@gmail.com>
2020-03-28 15:32:04 +0100

    added getMessage route

--- api/middlewares/router.js
+++ api/middlewares/router.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 const userRoutes = require('../routes/PersonneRoute');
 router.use('/user', userRoutes);
+
 const equipeRoutes = require('../routes/EquipeRoute');
 router.use('/equipe', equipeRoutes);
+
+const messageRoutes = require('../routes/MessageRoute');
+router.use('/message', messageRoutes);
 
 module.exports = router;
--- api/models/MessageModel.js
+++ api/models/MessageModel.js
@@ -1,6 +1,8 @@
 const pool = require('./dbConn');
 
 class MessageModel {
+    
+    /* cree un nouveau message */
     static setMessage = (idequipe,idpersonne,temporalite,texte) => {
         return new Promise((resolve, reject) => {
             pool.query('INSERT INTO `message` (id_equipe, id_personne, temporalite, texte) ' +
@@ -13,6 +15,21 @@ class MessageModel {
                     resolve(results.insertId);
                 }
             });
+        });
+    }
+
+    /* recupere tous les message  */
+    static getMessages = (idequipe) => {
+        return new Promise((resolve,reject) => {
+            pool.query('SELECT * FROM `message` '+
+                    'WHERE `id_equipe` = ?', idequipe, (err,results) => {
+                if(err) {
+                    reject(err);
+                } else {
+                    console.log('message recupere avec succes');
+                    resolve(results);
+                }
+           });
         });
     }
 };
