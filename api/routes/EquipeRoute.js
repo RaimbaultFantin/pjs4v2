39d118e4c516956d349f786faa605043000d7791
benjaminGUYT <55363637+benjaminGUYT@users.noreply.github.com>
2020-03-29 12:18:34 +0200

    Ajout /get/membres/:id
    retourne tous les membres (coachs + joueurs) d'une équipe

--- api/routes/EquipeRoute.js
+++ api/routes/EquipeRoute.js
@@ -71,6 +71,30 @@ router.get('/get/joueurs/:id', async(req, res) => {
     }
 })
 
+router.get('/get/membres/:id', async(req, res) => {
+    var token = req.headers['authorization'];
+    /*  On stock l'identifiant de l'utilisateur contenu dans le token */
+    var id = jwtUtils.checkToken(token);
+    console.log();
+    if (id < 0) {
+        return res.status(400).json(error(400, 'Token invalide'));
+    }
+
+    try {
+        let coachs = await model.getCoachs(req.params.id);
+        let joueurs = await model.getJoueurs(req.params.id);
+        return res.status(200).json({
+            'succes': 'succes',
+            'datas': {
+                'coachs': coachs,
+                'joueurs': joueurs
+            }
+        });
+    } catch (e) {
+        return res.status(500).json(error(500, e));
+    }
+})
+
 router.get('/get/bypersonne', async(req, res) => {
     var token = req.headers['authorization'];
     /*  On stock l'identifiant de l'utilisateur contenu dans le token */
