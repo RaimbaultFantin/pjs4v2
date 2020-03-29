39fe687c0fbf47ae131132bbbd2e2ce8a0324c79
benjaminGUYT <55363637+benjaminGUYT@users.noreply.github.com>
2020-03-29 13:57:01 +0200

    Ajout toute evenements

--- api/middlewares/router.js
+++ api/middlewares/router.js
@@ -5,5 +5,7 @@ const userRoutes = require('../routes/PersonneRoute');
 router.use('/user', userRoutes);
 const equipeRoutes = require('../routes/EquipeRoute');
 router.use('/equipe', equipeRoutes);
+const evenementRoutes = require('../routes/EvenementRoute');
+router.use('/evenement', evenementRoutes);
 
 module.exports = router;
